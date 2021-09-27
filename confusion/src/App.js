a60a426ca1522ba98a025c4736ba7d7aa7f5a356
RGiangregorio <ralph.a.giangregorio@gmail.com>
2021-09-27 16:05:02 -0400

    React router

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
 import './App.css';
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   
@@ -17,9 +15,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
