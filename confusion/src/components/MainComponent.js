35628d1fcd7f5cb1c9d1a1ed5108c5350f39087f
RGiangregorio <ralph.a.giangregorio@gmail.com>
2021-09-28 07:51:32 -0400

    AboutUs page

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -35,6 +36,14 @@ class Main extends Component {
       );
     }
 
+    const AboutUs = () => {
+      return(
+          <About 
+              leaders={this.state.leaders}
+          />
+      );
+  };
+
     const DishWithId = ({match}) => {
       return(
           <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
@@ -50,6 +59,7 @@ class Main extends Component {
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path='/contactus' component={Contact} />
+            <Route exact path="/aboutus" component={ AboutUs } />
             <Redirect to="/home" />
         </Switch>
         <Footer />
